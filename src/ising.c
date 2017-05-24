3572ae67aa763816f66a8c6ac1ef95077e307c2b
svletana <luciaa.fabio@gmail.com>
2017-05-24 19:03:17 -0300

    agregue temperatura como argumento

--- src/ising.c
+++ src/ising.c
@@ -7,40 +7,46 @@
 #include "lattice.h"
 
 int main(int argc, char **argv) {
-  float *params = (float *)malloc(2*sizeof(float));
-  float ener = 0;
-  float mag = 0;
-
-  *params = ener;
-  *(params+1) = mag;
-
-  FILE *output = fopen("output.txt", "w");
-  if (output == NULL)
-  {
-      fprintf(stderr, "Error opening file!\n");
-      exit(1);
+  if(argc!=2) {
+    printf("Error, ingresar un solo argumento.\n");
+    exit(1);
   }
+  else {
+    float *params = (float *)malloc(2*sizeof(float));
+    float ener = 0;
+    float mag = 0;
 
-  int n = 32;
-  int *lattice = (int *)malloc(n * n * sizeof(int));
- float prob = 0.5;
- float T = 0.1; //No poner T=0
- int niter = 10000;
- srand(time(NULL));
- float B = 1;
- float J = 1;
- float J2 = -1;
+    *params = ener;
+    *(params+1) = mag;
 
- params = fill_lattice(lattice, n, prob, ener, mag);
+    FILE *output = fopen("output.txt", "w");
+    if (output == NULL)
+    {
+        fprintf(stderr, "Error opening file!\n");
+        exit(1);
+    }
 
- for (int i = 0; i < niter; i++) {
-   params = metropolis(lattice, n, T, *params, *(params+1),B,J,J2);
-   //fprintf(output, "%f\n", prob );
-   fprintf(output, "%.10f %.10f\n", *(params+1)/(n*n), *params); //escribe: mag ener
+   int n = 32;
+   int *lattice = (int *)malloc(n*n*sizeof(int));
+   float prob = 0.5;
+   float T = atof(argv[1]); //No poner T=0
+   int niter = 1000000;
+   srand(time(NULL));
+   float B = 1;
+   float J = 1;
+   float J2 = 0;
 
- }
+   params = fill_lattice(lattice, n, prob, ener, mag);
 
-  fclose(output);
-  free(lattice);
-  return 0;
+   for (int i = 0; i < niter; i++) {
+     params = metropolis(lattice, n, T, *params, *(params+1),B,J,J2);
+     //fprintf(output, "%f\n", prob );
+    fprintf(output, "%.10f %.10f\n", *(params+1)/(n*n), *params); //escribe: mag ener
+
+   }
+
+    fclose(output);
+    free(lattice);
+    return 0;
+  }
 }
