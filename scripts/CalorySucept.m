e278074ade022cc0dcde421d3811b8846937cd97
svletana <luciaa.fabio@gmail.com>
2017-05-31 22:08:36 -0300

    datos y script para calor y suceptibilidad B=0 y J=1

--- scripts/CalorySucept.m
+++ scripts/CalorySucept.m
@@ -1,16 +1,9 @@
-C = zeros(1,30);
-X = zeros(1,30);
-temp = zeros(1,30);
-%calculo calor especifico y suceptibilidad
-for i=1:30
-  T = i*0.1;
-  temp(i) = T;
-  s = sprintf("/home/lusia/Documents/ising/bin/ising.e %d",T);
-  system(s);
-  load output.txt
-  mag = output(:,1);
-  ener = output(:,2);
-  
-  C(i) = (mean(ener.^2) - mean(ener)^2)/(T^2);
-  X(i) = (mean(mag.^2) - mean(mag)^2)/T;
-end
+load output.txt
+T = output(:,1);
+m = output(:,2);
+m2 = output(:,3);
+e = output(:,4);
+e2 = output(:,5);
+
+C = (e2-e.^2)./(T.^2);
+X = (m2-m.^2)./T;
