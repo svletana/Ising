e10091a0b5f436dcf574ee726fa2ce7efd58fa09
svletana <luciaa.fabio@gmail.com>
2017-06-06 10:35:05 -0300

    tests mejorados

--- tests/testflip.c
+++ tests/testflip.c
@@ -5,14 +5,14 @@
 #include "../src/lattice.h"
 #include "../src/metropolis.h"
 
-float calc(int k, int s, int n, int *red, float *params, float B, float J, float T);
+float calc(int k, int s, int n, int *red, float *params, float B, float J, float J2, float T);
 int *elegirred(int *red, int n, int s, int i);
 
 int main() {
   int *red = (int*)malloc(9 * sizeof(int));
   float *params = (float*)malloc(2 * sizeof(float));
   int s, n, i, k, trials;
-  float T, B, J, porcentaje;
+  float T, B, J, J2, porcentaje;
   srand(time(NULL));
 
   n = 3; // testeamos flip con redes de 3x3
@@ -20,6 +20,7 @@ int main() {
   T = 1.0;
   B = 0;
   J = 1;
+  J2 = 0;
   k = 5000; // numero de veces que se llama a flip
   trials = 4; // numero de test cases
   *params = 0;
@@ -27,23 +28,22 @@ int main() {
 
   for(i = 1; i <= trials; i++) {
     red = elegirred(red, n, s, i);
-    porcentaje = calc(k, s, n, red, params, B, J, T);
+    porcentaje = calc(k, s, n, red, params, B, J, J2, T);
     print_lattice(red, n, *params, *(params + 1));
-    printf("porcentaje de flips: %f%%\n", porcentaje);
+    printf("promedio valor del sitio: %f\n", porcentaje);
   }
   return 0;
 }
 
-float calc(int k, int s, int n, int *red, float *params, float B, float J, float T) {
+float calc(int k, int s, int n, int *red, float *params, float B, float J, float J2, float T) {
   int j;
   float porcentaje;
 
   for(j = 0; j < k; j++) {
-    //*(red + s) = 1; //deberia ser parametro
-    params = flip(red, s, n, T, params, B, J);
+    params = flip(red, s, n, T, params, B, J, J2);
     porcentaje += *(red + s);
   }
-  porcentaje = (100 * abs(porcentaje)) / k; // da una idea de la cantidad de veces que se flipea el spin
+  porcentaje = porcentaje / k; // da una idea de la cantidad de veces que se flipea el spin
   return porcentaje;
 }
 
